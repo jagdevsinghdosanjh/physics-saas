d41d0e1431208d6583cd10153e9c0c7379f6a06b
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2026-06-11 00:51:21 +0530

    Ok

--- server/prisma.config.js
+++ server/prisma.config.js
@@ -2,10 +2,8 @@ import "dotenv/config";
 
 export default {
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
   datasource: {
+    provider: "mysql",
     url: process.env.DATABASE_URL,
   },
 };
